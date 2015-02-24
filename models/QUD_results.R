723a8c7f534d0c295c54b816d203f98198c47749
gscontras <scontras@stanford.edu>
2015-02-24 08:59:32 -0800

    QUD plots

---
+++ models/QUD_results.R
@@ -0,0 +1,15 @@
+
+
+setwd("~/Documents/git/cocolab/collective/models/")
+
+d = read.csv("QUD_results.csv",header=T)
+
+d$noise<-factor(d$noise,levels=c("low",'mid','high'))
+
+d$QUD<-factor(d$QUD,levels=c('null','sum','min','max'))
+
+p <- ggplot(d,aes(x=QUD,y=pcollective,fill=noise)) +
+  geom_bar(stat='identity',position=position_dodge()) +
+  ylab("P(collective)")
+
+ggsave("QUD_results.png",plot=p,width=5, height=3)
