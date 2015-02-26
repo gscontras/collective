4e6d885f30b7e0daa524fa85b24e6b5699069324
gscontras <scontras@stanford.edu>
2015-02-26 14:32:50 -0800

    min QUD results

--- models/QUD_results.R
+++ models/QUD_results.R
@@ -6,11 +6,11 @@ d = read.csv("QUD_results.csv",header=T)
 
 d$noise<-factor(d$noise,levels=c("low",'mid','high'))
 
-d$QUD<-factor(d$QUD,levels=c('null','flat','max','sum'))
+d$QUD<-factor(d$QUD,levels=c('null','flat','max','min','sum'))
 
 p <- ggplot(d,aes(x=QUD,y=pcollective,fill=noise)) +
   geom_bar(stat='identity',position=position_dodge()) +
   ylab("P(collective)") +
   facet_grid(.~QUDs,scales = "free_x")
 
-ggsave("QUD_results.png",plot=p,width=5, height=3)
+ggsave("QUD_results.png",plot=p,width=7, height=3)
