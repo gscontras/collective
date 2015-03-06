721498fe4f6fcc315017fc587acd878567abfd25
gscontras <scontras@stanford.edu>
2015-03-06 13:17:21 -0800

    faultless results

--- experiments/3-faultless-disagreement/analysis.R
+++ experiments/3-faultless-disagreement/analysis.R
@@ -38,27 +38,23 @@ summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
 
 setwd("~/Documents/git/cocolab/collective/experiments/3-faultless-disagreement/Submiterator-master")
 
-d = read.table("faultless-disagreement-trials.tsv",sep="\t",header=T)
-head(d)
+f = read.table("faultless-disagreement-trials.tsv",sep="\t",header=T)
+head(f)
+
+f = f[d$sense=="Yes",]
 
 
+faultless_casted = dcast(data=d, sentence ~ faultless, value.var="response",mean)
+faultless_casted$faultless = (faultless_casted$yes/faultless_casted$no)
 
-a_sent_casted = dcast(data=a, sentence + faultless~ sentence_type, value.var="response",mean,na.rm=T)
-a_sent_casted$CI.YMin.coll = a_sent_casted$coll - dcast(data=a, sentence+ faultless~ sentence_type, value.var="response",ci.low,na.rm=T)$coll
-a_sent_casted$CI.YMin.dist = a_sent_casted$dist - dcast(data=a, sentence+ faultless~ sentence_type, value.var="response",ci.low,na.rm=T)$dist
-a_sent_casted$CI.YMax.coll = a_sent_casted$coll + dcast(data=a, sentence+ faultless~ sentence_type, value.var="response",ci.high,na.rm=T)$coll
-a_sent_casted$CI.YMax.dist = a_sent_casted$dist + dcast(data=a, sentence+ faultless~ sentence_type, value.var="response",ci.high,na.rm=T)$dist
-
-a_sent_word_plot <- ggplot(a_sent_casted, aes(x=coll,y=dist,color=faultless)) +
+ggplot(faultless_casted, aes(x=no,y=yes)) +
   #  geom_point() +
   # geom_smooth() +
   #geom_errorbar(alpha=.3,aes(ymin=CI.YMin.dist,ymax=CI.YMax.dist)) +
   #geom_errorbarh(alpha=.3,aes(xmin=CI.YMin.coll,xmax=CI.YMax.coll)) +  
-  geom_abline(intercept=0,slope=1) +
+  geom_abline(intercept=1,slope=-1) +
   geom_text(size=2,alpha=.5,aes(label=sentence),angle=45) +
-  ylab("distributive paraphrase endorsement") +
-  xlab("collective paraphrase endorsement") +
+  ylab("faultless?") +
+  xlab("collective?")+
   ylim(0,1) +
   xlim(0,1)
-
-ggsave(filename='attested_sentence_plot.png',plot=a_sent_word_plot,width=8, height=8)
