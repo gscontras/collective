b3769b403de2150680c78800676e3e89fa7e0cc2
gscontras <scontras@stanford.edu>
2015-04-10 17:36:22 -0700

    model fitting

--- model/model_results.R
+++ model/model_results.R
@@ -64,14 +64,15 @@ ggsave("plots/plural-predication-state.pdf",height=5,width=40,limitsize=FALSE)
 ### test plots
 
 
-d = read.csv("plural-predication-20.csv",header=F)
+d = read.csv("plural-predication-1.csv",header=F)
 #colnames(d) <- c("noise","numobjs","knowledge","collective","obj1","obj2","p")
 colnames(d) <- c("noise","numobjs","knowledge","collective","obj1","obj2","obj3","p")
 #colnames(d) <- c("noise","numobjs","knowledge","collective","obj1","obj2","obj3","obj4","p")
 head(d)
 
 
-d$noise <-factor(d$noise,levels=c("no","low",'mid','high'))
+d$noise <-factor(d$noise,levels=c("high","mid",'low','no'))
+#d$k <-factor(d$k,levels=c("partial","full"))
 d$numobjs <- factor(d$numobjs)
 #d$state = paste(d$obj1,d$obj2)
 d$state = paste(d$obj1,d$obj2,d$obj3)
@@ -79,19 +80,27 @@ d$state = paste(d$obj1,d$obj2,d$obj3)
 d$state <- factor(d$state)
 #d$KL <- factor(d$KL)
 d$p <- as.numeric(as.character(d$p))
+d$k = "partial"
+d[d$knowledge=="true",]$k = "full"
 head(d)
 
 # check effect direction for inferred thetas
 
-agg <- aggregate(p~noise*knowledge,d[d$collective=="true",],sum)
+agg <- aggregate(p~noise*k,d[d$collective=="true",],sum)
 
 p <- ggplot(agg,aes(x=noise,y=p)) +
-  geom_bar(stat='identity',position=position_dodge(),aes(fill=knowledge)) +
-  ylab("P(collective)") +
+  geom_bar(stat='identity',position=position_dodge(),aes(fill=k)) +
+  ylab("probability of\ncollective interpretation\n") +
+  xlab("\ncollective interpretation noise")+
+  labs(fill="speaker\nknowledge\naccess")+
+  scale_fill_manual(values=c("red", "blue"))+
   theme(axis.text.x = element_text(size=10,angle=0))#+
 #facet_grid(dist_theta~coll_theta)
 p
-ggsave("plots/plural-predication-20.pdf",height=3,width=8)
+
+setwd("~/Documents/git/CoCoLab/collective/writing/Cubert/plots/")
+
+ggsave("model-results.pdf")
 
 # check state distribution for inferred thetas
 
@@ -108,6 +117,59 @@ state
 ggsave("plots/plural-predication-20-state.pdf",height=5,width=40,limitsize=FALSE)
 
 
+
+### heavy plots
+
+
+d = read.csv("plural-predication-heavy.csv",header=F)
+#colnames(d) <- c("noise","numobjs","knowledge","collective","obj1","obj2","p")
+colnames(d) <- c("noise","numobjs","knowledge","collective","obj1","obj2","obj3","p")
+#colnames(d) <- c("noise","numobjs","knowledge","collective","obj1","obj2","obj3","obj4","p")
+head(d)
+
+
+d$noise <-factor(d$noise,levels=c("mid",'no'))
+#d$k <-factor(d$k,levels=c("partial","full"))
+d$numobjs <- factor(d$numobjs)
+#d$state = paste(d$obj1,d$obj2)
+d$state = paste(d$obj1,d$obj2,d$obj3)
+#d$state = paste(d$obj1,d$obj2,d$obj3,d$obj4)
+d$state <- factor(d$state)
+#d$KL <- factor(d$KL)
+d$p <- as.numeric(as.character(d$p))
+d$k = "partial"
+d[d$knowledge=="true",]$k = "full"
+head(d)
+
+# check effect direction for inferred thetas
+
+agg <- aggregate(p~noise*k,d[d$collective=="true",],sum)
+agg
+
+
+# a=2     .1414
+# a=1.8   .1451
+# a=1.75  .1462
+# a=1.6   .1488
+# a=1.5   .1501
+# a=1.4   .1507
+# a=1.375 .
+# a=1.35  .1507
+# a=1.3   .1504
+
+p <- ggplot(agg,aes(x=noise,y=p)) +
+  geom_bar(stat='identity',position=position_dodge(),aes(fill=k)) +
+  ylab("probability of\ncollective interpretation\n") +
+  xlab("\ncollective interpretation noise")+
+  labs(fill="speaker\nknowledge\naccess")+
+  scale_fill_manual(values=c("red", "blue"))+
+  theme(axis.text.x = element_text(size=10,angle=0))#+
+#facet_grid(dist_theta~coll_theta)
+p
+
+setwd("~/Documents/git/CoCoLab/collective/writing/Cubert/plots/")
+
+ggsave("model-results.pdf")
 
 
 
