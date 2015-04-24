89559412d137a1e51e460750e1ebfa05d8a66aaf
gscontras <scontras@stanford.edu>
2015-04-23 17:07:56 -0700

    results and notes

--- experiments/5-cubert-tense/analysis.R
+++ experiments/5-cubert-tense/analysis.R
@@ -28,8 +28,6 @@ d$collective = (d$coll / (d$coll + d$dist))
 d$collective_ratio = (d$coll / d$dist)
 d$coll_diff = (d$coll - d$dist)
 
-# tests
-
 aggregate(coll~context*utterance,data=d,mean)
 
 #aggregate(coll~context*tense*utterance,data=d,mean)
@@ -88,7 +86,7 @@ all_plot <- ggplot(all_d_s, aes(x=factor(context,labels=c("high","low")),y=respo
   facet_grid(sceanrio ~ utterance) #+ theme_blackDisplay() #+ theme(legend.position="bottom", legend.margin=unit(-.7,"cm") ) 
 all_plot
 
-ggsave("all_results.pdf",height=3.4)
+#ggsave("all_results.pdf",height=3.4)
 
 pres_s = bootsSummary(data=all_d[all_d$tense=="present",], measurevar="response", groupvars=c("context","utterance","sceanrio","sentence_type"))
 
@@ -118,7 +116,22 @@ past_plot <- ggplot(past_s, aes(x=factor(context,labels=c("high","low")),y=respo
   facet_grid(sceanrio ~ utterance) #+ theme_blackDisplay() #+ theme(legend.position="bottom", legend.margin=unit(-.7,"cm") ) 
 past_plot
 
-pdf("tense_plots.pdf")
+#pdf("tense_plots.pdf")
+#grid.arrange(pres_plot, past_plot)
+#dev.off()
+
 grid.arrange(pres_plot, past_plot)
-dev.off()
-s
+
+tense_s = bootsSummary(data=all_d, measurevar="response", groupvars=c("tense","sentence_type","utterance","context"))
+
+tense_plot <- ggplot(tense_s, aes(x=tense,y=response,fill=sentence_type)) +
+  #  geom_bar(alpha=1/2,stat="identity",position=position_dodge()) +
+  geom_bar(stat="identity",position=position_dodge()) +
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=tense, width=0.1),position=position_dodge(width=0.9))+
+  ylab("endorsement \n")+
+  #ylim(0,1)+
+  xlab("\n tense") +
+ # labs(title="past tense\n",fill="paraphrase")+
+  scale_fill_manual(values=c("red","blue"))+
+  facet_grid(context ~ utterance) #+ theme_blackDisplay() #+ theme(legend.position="bottom", legend.margin=unit(-.7,"cm") ) 
+tense_plot
