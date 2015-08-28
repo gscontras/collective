b36bf7b4639e21d84e89e67b9e18dff2f1ba651b
gscontras <scontras@stanford.edu>
2015-08-28 10:48:01 -0700

    new Cubert draft

--- writing/Cubert/plots/noise-plots.R
+++ writing/Cubert/plots/noise-plots.R
@@ -8,47 +8,32 @@ setwd("~/Documents/git/CoCoLab/collective/writing/Cubert/plots/")
 n = read.csv("noise.csv",header=T)
 
 n$level <- factor(n$level,levels=c("no","low","medium","high"))
+n$level <- factor(n$level,labels=c("no noise","low noise","medium noise","high noise"))
 
-ggplot(n,aes(x=value,y=p,color=level)) +
-  geom_point(alpha=1) +
-  geom_smooth(alpha=.25) +
-  xlab("noise value")+
-  ylab("probability")+
-  labs(fill="noise level")
-
-low <- ggplot(n, aes(x=value,y=p,fill=level)) +
-  #geom_bar(alpha=.5,stat='identity',position='stack')
-  geom_bar(stat="identity")
-  #geom_density(aes(alpha=0.5,size=1))
-  #ylab("") +
-  #xlab("")+
-  #ylim(0,1)
-low
+ggplot(n, aes(x=value,y=p)) +
+  geom_bar(stat="identity",width=.05) +
+  #scale_x_continuous(lim=c(0.38,1.62),breaks=c(0.5,0.7,1,1.3,1.5)) +
+  scale_x_continuous(breaks=round(seq(from=0,to=round(max(n$value),1),by=.2),1))+
+  scale_y_continuous(breaks=seq(0,1.24,by=.25)) +
+  geom_text(aes(label=value,y=p+.075),size=2.6) +
+  ylab("p(noise value)\n")+
+  xlab("\nnoise value")+
+  facet_wrap(~level,nrow=1)
+ggsave("noise_plots.pdf",height=2.25,width=6.5)
 
 
-qplot(value, ..count.., data=n, geom="density", fill=level, position="stack")
+s = read.csv("states.csv",header=T)
 
-#ggsave(filename='low.png',plot=low,width=5, height=3)
+s$level <- factor(s$level,labels=c("full knowledge","partial knowledge"))
 
-mid <- ggplot(data.frame(y=c(.2, .6, .2),
-                         x=c(0.5, 1, 1.5) ),
-              aes(x=x, y=y)) +
-  geom_bar(alpha=.5,fill="#17B32B",stat='identity',position=position_dodge())+
-  ylab("") +
-  xlab("")+
-  ylim(0,1)+
-  theme_blackDisplay()+
-  
-  
-  ggsave(filename='mid.png',plot=mid,width=5, height=3)
-
-high <- ggplot(data.frame(y=c(.333, .333, .333),
-                          x=c(0.5, 1, 1.5) ),
-               aes(x=x, y=y)) +
-  geom_bar(alpha=.5,fill="#5187FF",stat='identity',position=position_dodge())+
-  ylab("") +
-  xlab("")+
-  ylim(0,1)+
-  theme_blackDisplay()
-
-ggsave(filename='high.png',plot=high,width=5, height=3)
+ggplot(s, aes(x=state,y=p)) +
+  geom_bar(stat="identity") +
+  #scale_x_continuous(lim=c(0.38,1.62),breaks=c(0.5,0.7,1,1.3,1.5)) +
+  #scale_x_continuous(breaks=round(seq(from=0,to=round(max(n$value),1),by=.2),1))+
+  #scale_y_continuous(breaks=seq(0,1.24,by=.25)) +
+  #geom_text(aes(label=value,y=p+.075),size=2.6) +
+  ylab("p(state)\n")+
+  xlab("\nstate")+
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))+
+  facet_wrap(~level,nrow=1)
+ggsave("state_plots.pdf",height=2.25,width=6.5)
